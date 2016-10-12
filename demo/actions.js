9c2f24a2d799fc459b55905ae1ecb79ea6b652f6
Menon, Dhanya <dhanya.menon@pearson.com>
2016-10-11 19:01:37 -0700

    switching bookmarks to use store

---
+++ demo/actions.js
@@ -0,0 +1,18 @@
+export function addBookmark(bookmark) {
+  return {
+    type: 'ADD_BOOKMARK',
+    id: bookmark.id,
+    uri: bookmark.uri,
+    data: bookmark.data,
+    createdTimestamp: bookmark.createdTimestamp,
+    title: bookmark.title,
+    labels: bookmark.title
+  }
+}
+
+export function removeBookmark(id) {
+  return {
+    type: 'REMOVE_BOOKMARK',
+    id: id
+  }
+}
