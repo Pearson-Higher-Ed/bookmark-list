1e08043780da10cf6fd863180ab4919f6bbe0028
Menon, Dhanya <dhanya.menon@pearson.com>
2017-08-05 17:34:18 -0700

    update state when props change plus lint error fixes

--- src/js/BookmarkList.jsx
+++ src/js/BookmarkList.jsx
@@ -1,7 +1,7 @@
 /**
-PEARSON PROPRIETARY AND CONFIDENTIAL INFORMATION SUBJECT TO NDA
- *  Copyright © 2017 Pearson Education, Inc.
- *  All Rights Reserved.
+ * PEARSON PROPRIETARY AND CONFIDENTIAL INFORMATION SUBJECT TO NDA
+ * Copyright © 2017 Pearson Education, Inc.
+ * All Rights Reserved.
  *
  * NOTICE:  All information contained herein is, and remains
  * the property of Pearson Education, Inc.  The intellectual and technical concepts contained
@@ -10,7 +10,7 @@ PEARSON PROPRIETARY AND CONFIDENTIAL INFORMATION SUBJECT TO NDA
  * Dissemination of this information, reproduction of this material, and copying or distribution of this software
  * is strictly forbidden unless prior written permission is obtained
  * from Pearson Education, Inc.
-**/
+ */
 
 import React from 'react';
 import PropTypes from 'prop-types';
@@ -32,6 +32,12 @@ class BookmarkList extends React.Component {
       modalOpen: false,
       bookmarkId: ''
     };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.bookmarksArr !== this.props.bookmarksArr) {
+      this.setState({ bookmarkList: this.props.bookmarksArr });
+    }
   }
 
   handleModalOpen(id, e) {
--- src/js/InternationalSupport.jsx
+++ src/js/InternationalSupport.jsx
@@ -1,7 +1,7 @@
 /**
-PEARSON PROPRIETARY AND CONFIDENTIAL INFORMATION SUBJECT TO NDA
- *  Copyright © 2017 Pearson Education, Inc.
- *  All Rights Reserved.
+ * PEARSON PROPRIETARY AND CONFIDENTIAL INFORMATION SUBJECT TO NDA
+ * Copyright © 2017 Pearson Education, Inc.
+ * All Rights Reserved.
  *
  * NOTICE:  All information contained herein is, and remains
  * the property of Pearson Education, Inc.  The intellectual and technical concepts contained
@@ -10,7 +10,7 @@ PEARSON PROPRIETARY AND CONFIDENTIAL INFORMATION SUBJECT TO NDA
  * Dissemination of this information, reproduction of this material, and copying or distribution of this software
  * is strictly forbidden unless prior written permission is obtained
  * from Pearson Education, Inc.
-**/
+ */
 
 // language jsons
 import enJson from '../../translations/en.json';
